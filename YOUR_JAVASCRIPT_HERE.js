6e104af19dabb669c6c14125c88052e59e2bb45f
DavidB59 <david.behal@net-c.com>
2018-10-20 19:42:07 +0200

    displaying once only

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -36,31 +36,46 @@ function equipWeapon (hero) {
     console.log('function equipWeapon');
 
 };
-
+let displayed = 0;
 function displayStats () {
-    let liste1 = document.createElement("ul");
-    let heroName = document.createElement("li");
-    let heroic = document.createElement("li");
-    let inventory = document.createElement("li");
-    let health = document.createElement("li");
+    if (displayed === 0){
+        let liste1 = document.createElement("ul");
+        let heroName = document.createElement("li");
+        let heroic = document.createElement("li");
+        let inventory = document.createElement("li");
+        let health = document.createElement("li");
+    
+        heroName.id="heroName";
+        heroic.id="heroic";
+        inventory.id="inventory";
+        health.id="health";
+        let displayStat = document.getElementById("displayStat");
+    
+        heroName.innerText = 'Name :' + hero.name 
+        heroic.innerText = 'heroic :' + hero.heroic
+        inventory.innerText = 'inventory :'+ hero.inventory[0].type
+        health.innerText = 'health :'+hero.health
+    
+        console.log('calling display stats')
+        console.log(heroName.innerText)
+    
+        displayStat.appendChild(liste1);
+    
+        liste1.appendChild(heroName);
+        liste1.appendChild(heroic);
+        liste1.appendChild(inventory);
+        liste1.appendChild(health);
+    
+        displayed = 1   
+    }
 
-
-    let displayStat = document.getElementById("displayStat");
-
-    heroName.innerText = 'Name :' + hero.name 
-    heroic.innerText = 'heroic :' + hero.heroic
-    inventory.innerText = 'inventory :'+ hero.inventory[0].type
-    health.innerText = 'health :'+hero.health
-
-    console.log('calling display stats')
-    console.log(heroName.innerText)
-
-    displayStat.appendChild(liste1);
-
-    liste1.appendChild(heroName);
-    liste1.appendChild(heroic);
-    liste1.appendChild(inventory);
-    liste1.appendChild(health);
+    if (displayed ===1) {
+        heroName.innerText = 'Name :' + hero.name 
+        heroic.innerText = 'heroic :' + hero.heroic
+        inventory.innerText = 'inventory :'+ hero.inventory[0].type
+        health.innerText = 'health :'+hero.health
+    }
+   
 
 
     //let heroName = document.createElement("h2");
