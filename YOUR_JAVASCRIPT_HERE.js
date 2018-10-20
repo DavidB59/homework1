eb510f47ff4097e6d4145ac1d7fb851059c62393
DavidB59 <david.behal@net-c.com>
2018-10-20 19:23:44 +0200

    displaying

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -36,3 +36,33 @@ function equipWeapon (hero) {
     console.log('function equipWeapon');
 
 };
+
+function displayStats () {
+    let liste1 = document.createElement("ul");
+    let heroName = document.createElement("li");
+    let heroic = document.createElement("li");
+    let inventory = document.createElement("li");
+    let health = document.createElement("li");
+
+
+    let displayStat = document.getElementById("displayStat");
+
+    heroName.innerText = 'Name :' + hero.name 
+    heroic.innerText = 'heroic :' + hero.heroic
+    inventory.innerText = 'inventory :'+ hero.inventory
+    health.innerText = 'health :'+hero.health
+
+    console.log('calling display stats')
+    console.log(heroName.innerText)
+
+    displayStat.appendChild(liste1);
+
+    liste1.appendChild(heroName);
+    liste1.appendChild(heroic);
+    liste1.appendChild(inventory);
+    liste1.appendChild(health);
+
+
+    //let heroName = document.createElement("h2");
+
+}
